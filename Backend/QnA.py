64a928ef62dff1e9634afc3b31c9b2a0cc1c96fc
kevin.orathel <kevin.orathel@gmail.com>
2025-09-21 14:52:09 -0400

    Feat: Added basic ChatBot funcitionality

--- Backend/QnA.py
+++ Backend/QnA.py
@@ -1,20 +1,37 @@
 import os
-from huggingface_hub import InferenceClient
-from PDFScanner import read_scanned_pdf
 
-client = InferenceClient(
-    provider="hf-inference",
-    api_key=os.environ["HF_TOKEN"],
-)
+import google.generativeai as genai
+from PDFUtil import read_scanned_pdf
+from test import search_index, read_scanned_pdf, chunk_text, embed_chunks, build_faiss_index
 
-ques = input("Enter your question: ")
-file_path = r"C:\Users\kevin\OneDrive\Desktop\College\Clark U\Design and Analysis of Algorithms\Chapter 1\1.1 What is an algorithm.pdf"
-context = read_scanned_pdf(file_path)
+api_key = os.environ["GEMINI_API_KEY"]
 
-answer = client.question_answering(
-    question=ques,
-    context=context,
-    model="timpal0l/mdeberta-v3-base-squad2",
-)
+question = input("Enter your question: ")
+file_path = r"C:\Users\kevin\OneDrive\Desktop\College\Clark U\Design and Analysis of Algorithms\Chapter 1\Introduction to Graph theory.pdf"
 
-print(answer.answer)
+text = read_scanned_pdf(file_path)
+chunks = chunk_text(text, chunk_size=200, overlap=50)
+vectors = embed_chunks(chunks)
+index = build_faiss_index(vectors)
+results = search_index(question, index, chunks, k=3)
+context = results[0][0]
+
+
+genai.configure(api_key=api_key)
+model = genai.GenerativeModel(model_name="gemini-2.5-flash")
+
+prompt = f"""
+    You are a helpful assistant. Use the provided context to answer the question. 
+    Make sure that the answer is explained in a way that is easy to understand.
+    If the answer cannot be found in the context, say "I don’t know based on the given document."
+    
+    Context:
+    {context}
+    
+    Question:
+    {question}
+    """
+
+response = model.generate_content(prompt)
+
+print(response.text)
