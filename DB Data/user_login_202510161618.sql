9120d7882d28233bdd933d4257653cf658b706b9
kevin.orathel <kevin.orathel@gmail.com>
2025-10-24 00:43:12 -0400

    Backup: Added DB Data for backup

---
+++ DB Data/user_login_202510161618.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.user_login (first_name,last_name,email,pwd,is_verified) VALUES
+	 ('Prof','Sufiyan','doctor.sufiyan@gmail.com','password',true),
+	 ('Kevin','Joseph','kevin@gmail.com','passwrd',true),
+	 ('S','B','sb@gmail.com','$2b$12$d2tCQnX7r1lQR9lHPtRbwup0WHfeufi.DuYDawVNpzlbI1GQ2.bpy',false),
+	 ('dummy','data','ddata@gmail.com','$2b$12$5A7qYOqR3u6DFCdjiKRF1u6fm7tLb0d0e2wZ3obVHSU0WD2rIhOLi',false),
+	 ('vikas','f','vf@gmail.com','$2b$12$7vvhceyz9aPVllL3u2z6r.zi3sryWmUHFtV6VrHJzmlTIHPHkpnRu',false),
+	 ('J','D','jd@gmail.com','$2b$12$C9ye5208.iA3miphJ8nRkuDlPTcNuzBW7rQloNYHsEVf3cTtgixsm',false),
+	 ('Kevin','J','kj@gmail.com','$2b$12$wNTYhXL9TXP022ndlFkTCOt.08C6hQtESXf05yLEz9XvZamFXoIyS',false);
