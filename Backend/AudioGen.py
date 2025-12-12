e99dab55bdfef34af3c2696d83a03866ee4b6128
kevin.orathel <kevin.orathel@gmail.com>
2025-12-11 21:58:09 -0500

    Fix: Removed the audio limit

--- Backend/AudioGen.py
+++ Backend/AudioGen.py
@@ -229,8 +229,8 @@ def blocking_audio_generation_task(session_id: int):
             cur.execute("SELECT e.chunk_text FROM embeddings e LEFT JOIN sessiondocuments sd ON sd.document_id  = e.document_id WHERE sd.session_id = %s;", (session_id,))
             session_chunks = cur.fetchall()
 
-            if len(session_chunks) > 30:
-                return None, None
+            # if len(session_chunks) > 30:
+            #     return None, None
 
             for i, chunk in enumerate(session_chunks):
 
